62695d71d8615718ce270156a5518caaa143eeaa
vibhu2408 <vaibhavgupta24885@gmail.com>
2023-01-31 08:55:52 +0530

    First Commit

---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,33 @@
+import Post from "../post/Post";
+import "./posts.scss";
+
+const Posts = () => {
+  //TEMPORARY
+  const posts = [
+    {
+      id: 1,
+      name: "Vaibhav Gupta",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "The company itself is a very successful company",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "Vaibhav Gupta",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "He is obliged to compensate the pain of the pleasures of the thing with the pleasure of convenience, for the pleasures are the laborious consequences of ours! You don't get bigger than that in the smallest amount of time.",
+    },
+  ];
+
+  return <div className="posts">
+    {posts.map(post=>(
+      <Post post={post} key={post.id}/>
+    ))}
+  </div>;
+};
+
+export default Posts;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
+
+const Login = () => {
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = () => {
+    login();
+  };
+
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Namaste World.</h1>
+          <p>
+            The company itself is a very successful company. free with
+            otherwise we never deserve the whole exercise itself, for it is a mistake,
+            result
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
